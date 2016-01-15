8c7bc553dd5f15356102799b4415db08a9d2e41f
Damien Kelly <damienkelly@google.com>
2016-01-14 17:37:13 -0800

    Enable spatial audio metadata injection for mp4 files.

--- spatialmedia/mpeg/constants.py
+++ spatialmedia/mpeg/constants.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 Google Inc. All rights reserved.
+# Copyright 2016 Google Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,6 +30,8 @@ TAG_MDAT = "mdat"
 TAG_XML = "xml "
 TAG_HDLR = "hdlr"
 TAG_FTYP = "ftyp"
+TAG_ESDS = "esds"
+TAG_SOUN = "soun"
 
 # Container types.
 TAG_MOOV = "moov"
@@ -37,15 +39,20 @@ TAG_UDTA = "udta"
 TAG_META = "meta"
 TAG_TRAK = "trak"
 TAG_MDIA = "mdia"
+TAG_MP4A = "mp4a"
 TAG_MINF = "minf"
 TAG_STBL = "stbl"
+TAG_STSD = "stsd"
 TAG_UUID = "uuid"
+TAG_SA3D = "SA3D"
 
 CONTAINERS_LIST = [
     TAG_MDIA,
     TAG_MINF,
+    TAG_MP4A,
     TAG_MOOV,
     TAG_STBL,
+    TAG_STSD,
     TAG_TRAK,
     TAG_UDTA,
     ]
